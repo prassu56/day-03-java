b516bbc254f11ec34b9ca49cd3541ffd83ff16f6
prassu56 <prasuprasanna236@gmail.com>
2024-12-04 12:44:35 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Multiple{
+    public static void main(String[]args){
+        int a=18;
+        if((a&3)==0){
+            System.out.println("multiple of 4");
+        }else{
+             System.out.println("not a multiple of 4");
+        }
+    }
+}
