38d587e3021f3974588ded46d22de0c480395ad2
prassu56 <prasuprasanna236@gmail.com>
2024-12-04 14:31:08 +0530

    Add files via upload

---
+++ Greatestofthreenumbers.java
@@ -0,0 +1,20 @@
+public class Greatestofthreenumbers{
+    public static void main(String[]args){
+        int a=10;
+        int b=2;
+        int c=8;
+        if((a>b)&&(a>c))
+        {
+            System.out.println("a  is greater");
+        }
+        else if((b>a)&&(b>c))
+        {
+             System.out.println("b  is greater");
+        }
+        else
+        {
+             System.out.println("c  is greater");
+        }
+
+    }
+}
