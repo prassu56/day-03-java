74ca2b1ee6993f754d25aeef720651b61582f966
prassu56 <prasuprasanna236@gmail.com>
2024-12-04 10:17:34 +0530

    Add files via upload

---
+++ Ifelsebiteven.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Ifelsebiteven{
+    public static void main(String[]args){
+        int a=3;
+        if((a&1)==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+        }
+    }
